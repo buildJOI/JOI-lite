8b710957dc5b1f1065c7c24d266ae3b4660008c3
buildJOI <jithnjvn@gmail.com>
2025-05-16 13:22:04 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def joi_chat(user_input):
+  #simple fallback response for now
+  return"Hey there, joe! How can i brighten your day?"
+
+def main():
+  print("Welcome to JOI-lite :D")
+  while True:
+    user_text=input("You:)
+                    if user_text.lower()in['exit','quit','see ya later']
+                    print("JOI: See you later, love!")
+    break
+                    response=joi_chat(user_text)
+                            print(f"JOI:{response}")
+    if__name__=="__main__":
+                            main()
